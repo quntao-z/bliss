e8260736ff8acd307d20e3120f3b03eca93691a0
QW66666 <wusteven0829@gmail.com>
2023-10-29 17:19:58 -0400

    resolve merge conflicts

--- src/main/java/com/bliss/blissapp/Repository/QuoteRepository.java
+++ src/main/java/com/bliss/blissapp/Repository/QuoteRepository.java
@@ -2,12 +2,8 @@ package com.bliss.blissapp.Repository;
 
 import com.bliss.blissapp.Model.Quote;
 import org.springframework.data.mongodb.repository.MongoRepository;
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 
 @Repository
-=======
-
->>>>>>> main
 public interface QuoteRepository extends MongoRepository<Quote, Long> {
 }
