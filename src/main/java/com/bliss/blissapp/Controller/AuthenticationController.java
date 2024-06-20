1743139c97514bf4c4ddb1358c8732b1991f0baf
QW66666 <wusteven0829@gmail.com>
2024-06-20 13:50:34 -0400

    added response to obtain whether token is expired

--- src/main/java/com/bliss/blissapp/Controller/AuthenticationController.java
+++ src/main/java/com/bliss/blissapp/Controller/AuthenticationController.java
@@ -4,20 +4,19 @@ import com.bliss.blissapp.Model.AuthenticationRequest;
 import com.bliss.blissapp.Model.AuthenticationResponse;
 import com.bliss.blissapp.Model.RegisterRequest;
 import com.bliss.blissapp.Service.AuthenticationService;
+import com.bliss.blissapp.Service.JwtService;
 import jdk.jfr.Registered;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private final JwtService jwtService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
@@ -27,6 +26,11 @@ public class AuthenticationController {
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(this.authenticationService.authenticate(request));
+    }
+
+    @GetMapping("/expired")
+    public ResponseEntity<Boolean> isTokenExpired(@RequestParam String token) {
+        return ResponseEntity.ok(this.jwtService.isTokenExpired(token));
     }
 
 }
